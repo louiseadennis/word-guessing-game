7e3dc9404f9e55dfdf8bb118e7ac6a1a4d496ac4
t95229ld <louise.dennis@manchester.ac.uk>
2022-03-06 15:46:04 +0000

    Removing a warning.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { InfoModal } from './components/modals/InfoModal'
 import { StatsModal } from './components/modals/StatsModal'
 import { SettingsModal } from './components/modals/SettingsModal'
 import {
-  GAME_TITLE,
   GAME_COPIED_MESSAGE,
   NOT_ENOUGH_LETTERS_MESSAGE,
   WORD_NOT_FOUND_MESSAGE,
