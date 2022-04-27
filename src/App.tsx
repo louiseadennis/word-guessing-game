2f6ffa99133279e7b0443216114dc0530937f03d
t95229ld <louise.dennis@manchester.ac.uk>
2022-04-27 19:35:48 +0100

    Fixing padding finally.

--- src/App.tsx
+++ src/App.tsx
@@ -258,7 +258,7 @@ function App() {
         setIsSettingsModalOpen={setIsSettingsModalOpen}
       />
       <div className="pt-2 px-1 pb-8 md:max-w-7xl w-full mx-auto sm:px-6 lg:px-8 flex flex-col grow">
-        <div className="pb-1 grow">
+        <div className="pb-0 grow">
           <Grid
             guesses={guesses}
             currentGuess={currentGuess}
